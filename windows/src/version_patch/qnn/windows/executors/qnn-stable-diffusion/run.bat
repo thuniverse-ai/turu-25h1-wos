823232f45f8c5e0c679c0eb6dcd58900b5a9c0e0
Yung-Hsiang Hu <iftnt1999@gmail.com>
2025-05-29 08:01:49 +0800

    [exec, win] add: Advanced QNN-based exeutor

--- windows/src/version_patch/qnn/windows/executors/qnn-stable-diffusion/run.bat
+++ windows/src/version_patch/qnn/windows/executors/qnn-stable-diffusion/run.bat
@@ -5,6 +5,4 @@ set EXECUTOR_ACCESS_CODE=.model:qnn/qualcomm/stable-diffusion
 pushd ..\..\..\src\multi-chat
 php artisan model:config ".model:qnn/qualcomm/stable-diffusion" "Painter @NPU" --order=120001 --image "..\..\windows\executors\qnn-stable-diffusion\sd_qnn.png"
 popd
-pushd ..\..\..\src\executor\image_generation
-start /b "" "python" .\qnn_stable_diffusion.py "--access_code" ".model:qnn/qualcomm/stable-diffusion" "--model" "qualcomm/Stable-Diffusion-v1.5" "--qnn_binaries" "..\..\..\windows\packages\qnn-binaries" --log debug
-popd
+start /b "" "python" .\main.py "--access_code" ".model:qnn/qualcomm/stable-diffusion" "--model_version" "SD_1_5"
